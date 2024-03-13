0f0ccdfc8a3e6ef108cf63a8dce8049f23e2788e
Mattdevill <110395675+Mattdevill@users.noreply.github.com>
2024-03-13 11:23:31 +0200

    IN SPACE!!

--- GADE Catch the flag/Assets/Scri[ts/ScoringSystem.cs
+++ GADE Catch the flag/Assets/Scri[ts/ScoringSystem.cs
@@ -13,7 +13,7 @@ public class ScoringSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerScoreText.text = "Player:" + playerScore;
+        //playerScoreText.text = "Player:" + playerScore;
     }
 
     // Update is called once per frame
@@ -22,12 +22,14 @@ public class ScoringSystem : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if(collision.transform.name == "RedBase")
+        if (other.CompareTag("BlueFlag"))
         {
             
+            playerScore ++;
         }
+        Debug.Log("Point Test");
     }
 
 }
