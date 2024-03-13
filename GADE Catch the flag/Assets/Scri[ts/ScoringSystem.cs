8406f8f1f0535217f11ff3383c7fa0cff288e218
Mattdevill <110395675+Mattdevill@users.noreply.github.com>
2024-03-13 14:11:49 +0200

    stuff

--- GADE Catch the flag/Assets/Scri[ts/ScoringSystem.cs
+++ GADE Catch the flag/Assets/Scri[ts/ScoringSystem.cs
@@ -6,28 +6,31 @@ using TMPro;
 public class ScoringSystem : MonoBehaviour
 {
     public TextMeshProUGUI playerScoreText;
-    public int playerScore;
+    public int playerScore = 0;
 
     //public static int blueScore = 0;
     //public static int redScore = 0;
     // Start is called before the first frame update
     void Start()
     {
-        //playerScoreText.text = "Player:" + playerScore;
+        playerScoreText.text = "Player:" + playerScore;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        playerScoreText.text = "Player:" + playerScore;
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("BlueFlag"))
-        {
-            
-            playerScore ++;
+        { 
+           playerScore++;
+           playerScoreText.text = "Player:" + playerScore;
+
         }
         Debug.Log("Point Test");
     }
