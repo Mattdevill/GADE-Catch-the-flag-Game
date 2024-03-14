45e864769c6a8cc862abf5899ffe6b828b161796
Mattdevill <110395675+Mattdevill@users.noreply.github.com>
2024-03-14 14:50:42 +0200

    added states

--- GADE Catch the flag/Assets/Scri[ts/StateManager.cs
+++ GADE Catch the flag/Assets/Scri[ts/StateManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class StateManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    State currentState;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        RunStateMachine();
+    }
+
+    private void RunStateMachine()
+    {
+        State nextState = currentState?.RunCurrentState();
+
+        if (nextState != null )
+        {
+            SwitchToNextState(nextState);
+        }
+    }
+
+    private void SwitchToNextState(State nextState)
+    {
+        currentState = nextState;
     }
 }
