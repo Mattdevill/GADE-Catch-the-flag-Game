c523632124d5b09f9640290d7deccafe3d687a24
Mattdevill <110395675+Mattdevill@users.noreply.github.com>
2024-03-15 16:29:08 +0200

    added enemy movement

--- GADE Catch the flag/Assets/Scri[ts/EnemyAIScripts/ChaseState.cs
+++ GADE Catch the flag/Assets/Scri[ts/EnemyAIScripts/ChaseState.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ChaseState : State
 {
     public AttackState attackState;
 
     public bool IsInAttackRange;
+
+    public NavMeshAgent agent;
+
+    public Transform player;
 
    // public Transform targetObject;
 
@@ -19,7 +24,9 @@ public class ChaseState : State
             return attackState;
         }
         else
-        {        
+        {
+            agent.SetDestination(player.position);
+
             /*self.transform.position = Vector3.MoveTowards(self.transform.position, targetObject.position, 10 * Time.deltaTime);
 
             Debug.Log("Test");*/
