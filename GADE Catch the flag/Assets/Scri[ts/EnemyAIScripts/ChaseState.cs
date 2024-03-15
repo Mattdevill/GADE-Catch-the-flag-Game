03cd7fc4640e85d60af64f1372a6385d241bae66
Mattdevill <110395675+Mattdevill@users.noreply.github.com>
2024-03-15 16:34:37 +0200

    enemy moves

--- GADE Catch the flag/Assets/Scri[ts/EnemyAIScripts/ChaseState.cs
+++ GADE Catch the flag/Assets/Scri[ts/EnemyAIScripts/ChaseState.cs
@@ -13,10 +13,6 @@ public class ChaseState : State
 
     public Transform player;
 
-   // public Transform targetObject;
-
-    //public GameObject self;
-
     public override State RunCurrentState()
     {
         if (IsInAttackRange)
@@ -27,18 +23,7 @@ public class ChaseState : State
         {
             agent.SetDestination(player.position);
 
-            /*self.transform.position = Vector3.MoveTowards(self.transform.position, targetObject.position, 10 * Time.deltaTime);
-
-            Debug.Log("Test");*/
-
             return this;
-
-           
         }
-    }
-
-    private void Update()
-    {
-
     }
 }
