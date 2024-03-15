a08f60d80976e220bdbf2a164192061811981362
Mattdevill <110395675+Mattdevill@users.noreply.github.com>
2024-03-15 17:12:59 +0200

    added enemy range

--- GADE Catch the flag/Assets/Scri[ts/EnemyAIScripts/ChaseState.cs
+++ GADE Catch the flag/Assets/Scri[ts/EnemyAIScripts/ChaseState.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class ChaseState : State
 {
     public AttackState attackState;
-
     public bool IsInAttackRange;
 
     public NavMeshAgent agent;
-
     public Transform player;
+
+    public float radius;
+    [Range(0,360)]
+    public float angle;
+    public LayerMask targetMask;
+    public LayerMask obctructionMask;
+    public bool canSeePlayer;
 
     public override State RunCurrentState()
     {
@@ -21,9 +27,44 @@ public class ChaseState : State
         }
         else
         {
-            agent.SetDestination(player.position);
+            FieldOfView();
+            
 
             return this;
+        }
+    }
+
+    public void FieldOfView()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if(rangeChecks.Length != 0 )
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obctructionMask))
+                {
+                    canSeePlayer = true;
+                    agent.SetDestination(player.position);
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+        }
+        else if (canSeePlayer)
+        {
+            canSeePlayer = false;
         }
     }
 }
