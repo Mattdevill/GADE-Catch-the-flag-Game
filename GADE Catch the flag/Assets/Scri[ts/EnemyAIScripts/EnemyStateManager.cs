2dffcf76605a98627e133ce39e4a0f3cb01cb1e8
Mattdevill <110395675+Mattdevill@users.noreply.github.com>
2024-03-20 16:42:41 +0200

    stuff

--- GADE Catch the flag/Assets/Scri[ts/EnemyAIScripts/EnemyStateManager.cs
+++ GADE Catch the flag/Assets/Scri[ts/EnemyAIScripts/EnemyStateManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,6 +10,16 @@ public class EnemyStateManager : MonoBehaviour
     //For FetchRedFlag State
     public NavMeshAgent agent;
     public Transform redFlag;
+
+    //For Chase State
+    public bool IsInAttackRange;
+    public bool canSeePlayer;
+    public Transform player;
+    public float radius;
+    [Range(0, 360)]
+    public float angle;
+    public LayerMask targetMask;
+    public LayerMask obctructionMask;
 
     // Define your player states here
     public enum EnemyState
@@ -41,7 +53,7 @@ public class EnemyStateManager : MonoBehaviour
         {
             SetState(EnemyState.Attack);
         }
-        else if (Input.GetKey(KeyCode.W))
+        else if (canSeePlayer == true)
         {
             SetState(EnemyState.Chase);
         }
@@ -73,12 +85,16 @@ public class EnemyStateManager : MonoBehaviour
         switch (currentState)
         {
             case EnemyState.FetchRedFlag:
-                // Code for idle state
+                // Code for FetchRedFag state
                 agent.SetDestination(redFlag.position);
 
                 break;
+
             case EnemyState.Chase:
-                // Code for walking state
+                // Code for chase state
+
+                 FieldOfView();
+               
                 break;
             case EnemyState.ReturnToBase:
                 // Code for jumping state
@@ -97,6 +113,49 @@ public class EnemyStateManager : MonoBehaviour
     private void EnterState()
     {
         // Implement any actions needed when entering a new state
-        Debug.Log("Entering State: " + currentState);
+        //Debug.Log("Entering State: " + currentState);
+    }
+
+    //Method to handle enemy view
+    public void FieldOfView()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obctructionMask))
+                {
+                    canSeePlayer = true;
+                    agent.SetDestination(player.position);
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+        }
+        else if (canSeePlayer)
+        {
+            canSeePlayer = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "RedFlag")
+        {
+            this.transform.parent = other.transform;
+        }
     }
 }
